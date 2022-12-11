767eaac455b2b9a5eb8000e0433a84621e95e6e6
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2022-12-11 13:51:44 +0100

    Add translation

---
+++ Translate.py
@@ -0,0 +1,20 @@
+#This the class that will translate the text from the user.
+
+class Translate:
+
+    languageToNum = ["english","french"]
+    constantToLanguage = {
+        "FIRE_COUNTRY": ("The Fire Country", "La Terre du Feu"),
+        "WATER_COUNTRY": ("The Water Country", "La Terre de l'Eau"),
+        "EARTH_COUNTRY": ("The Earth Country", "La Terre de la Terre"),
+        "AIR_COUNTRY": ("The Air Country", "La Terre de l'Air")
+    }
+    currentLanguage = languageToNum.index("english")
+
+    @staticmethod
+    def get(text):
+        return constantToLanguage[text][currentLanguage]
+
+    @staticmethod
+    def setLanguage(language):
+        currentLanguage = languageToNum.index(language)
