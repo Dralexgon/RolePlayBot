a9f15b7ec9e875cf4325372fa7ad7361a80749b7
Alexandre Pellereau <alexandre.pellereau@epita.fr>
2022-12-15 22:29:23 +0100

    Starting creating create_character command

--- Main.py
+++ Main.py
@@ -14,6 +14,20 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('pong')
 
+@bot.command()
+async def create_character(ctx):
+    #the user will respond by typing message in the same channel or by reacting to the message
+    await ctx.send("What is your character's name ?")
+    name = await bot.wait_for('message', check=lambda message: message.author == ctx.author and message.channel == ctx.channel)
+    await ctx.send("What is your character's age ?")
+    age = await bot.wait_for('message', check=lambda message: message.author == ctx.author and message.channel == ctx.channel)
+    question = await ctx.send("What is your character race ?")
+    #add emojis to choose
+    await question.add_reaction(':Human:')
+    await question.add_reaction(':Sangdedemon:')
+
+#note for me, to wait for an answer, use await bot.wait_for('message', check=lambda message: message.author == ctx.author and message.channel == ctx.channel)
+
 #note, if you want to run this code, you need to create a file called token.txt one directory above the code and put your token in it.
 token = open('../token.txt', 'r').readlines()[0]
 bot.run(token)
